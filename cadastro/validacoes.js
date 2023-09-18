0ef85685dce1522592606f9409eaad651d207c6e
Rafael <rafaelmarliere033@gmail.com>
2023-09-18 17:55:33 -0300

    pagina de cadastro tá filé mignon

--- cadastro/validacoes.js
+++ cadastro/validacoes.js
@@ -1,107 +1,134 @@
 // Declarando as varáveis globais
-
-let nome_usuario_valor = document.getElementById('iusuario');
-let email_valor = document.getElementById('iemail');
-let senha_valor = document.getElementById('isenha');
-let confirmar_senha_valor = document.getElementById('iconfirmarsenha'); 
-let botao_cadastro = document.getElementById('botao_cadastro');
+let nome_usuario = document.getElementById("iusuario");
+let email = document.getElementById("iemail");
+let senha_valor = document.getElementById("isenha");
+let confirmar_senha = document.getElementById("iconfirmarsenha");
+let botao_cadastro = document.getElementById("botao_cadastro");
+let msg_erro_nome_usuario = document.getElementById("msg_erro_nome_usuario");
+let msg_erro_email = document.getElementById("msg_erro_email");
+let msg_erro_senha = document.getElementById("msg_erro_senha");
+let msg_erro_confirmar_senha = document.getElementById("msg_erro_confirmar_senha");
 var return_nome_usuario;
 var return_email;
 var senha;
 var return_senha;
 var return_confirmar_senha;
 
-nome_usuario_valor.addEventListener('input', function() {
-    if (this.value.length == 0) {
-        // Mostre o erro de preencher o campo
-        
-        return_nome_usuario =  false;
+nome_usuario.addEventListener("input", function () {
+    if (this.value.length === 0) {
+        msg_erro_nome_usuario.setAttribute("style", "display: none");
+        msg_erro_nome_usuario.classList.remove("msg_erro");
     } else if (this.value.length < 3 || this.value.length > 16) {
         // Mostre o erro de que precisa estar entre 3 e 16 caracteres
+        msg_erro_nome_usuario.removeAttribute("style");
+        msg_erro_nome_usuario.classList.add("msg_erro");
+        msg_erro_nome_usuario.innerHTML = 'O campo "nome de usuário" deve conter entre 3 e 16 caracteres.';
 
-        return_nome_usuario =  false;
-    } else if (this.value.indexOf(' ') !== -1) {
+        return_nome_usuario = false;
+    } else if (this.value.indexOf(" ") !== -1) {
         // Mostre o erro de que contém espaços em branco
+        msg_erro_nome_usuario.removeAttribute("style");
+        msg_erro_nome_usuario.classList.add("msg_erro");
+        msg_erro_nome_usuario.innerHTML = 'O campo "nome de usuário" não pode conter espaços em branco.';
 
-        return_nome_usuario =  false;
+        return_nome_usuario = false;
     } else {
+        msg_erro_nome_usuario.setAttribute("style", "display: none");
+        msg_erro_nome_usuario.classList.remove("msg_erro");
+
         return_nome_usuario = true;
     }
 
     habilitar_botao();
 });
 
-email_valor.addEventListener('input', function() {
-    if (this.value.length == 0) {
-        // Mostre o erro de preencher o campo
+email.addEventListener("input", function () {
+    // Verifica se há algo escrito após o "@"
+    var regex = /@.+/;
 
-        return_email = false;
-    } else if (this.value.indexOf('@') === -1) {
+    if (this.value.length === 0) {
+        msg_erro_email.setAttribute("style", "display: none");
+        msg_erro_email.classList.remove("msg_erro");
+    } else if (this.value.indexOf("@") === -1) {
         // Mostre o erro de que precise ter @
+        msg_erro_email.removeAttribute("style");
+        msg_erro_email.classList.add("msg_erro");
+        msg_erro_email.innerHTML = 'O campo "email" deve conter um "@".';
 
         return_email = false;
-    } else if (this.value.indexOf(' ') == (this.value.indexOf('@')) + 1) {
-        // Mostre o erro de que depois do @ está em branco *OBS -> ORGANIZAR LÓGICA ERRADA!
-
-        console.log('erro de espaco em branco depois do @');
+    } else if (!regex.test(this.value)) {
+        // Mostre o erro de que depois do @ está em branco
+        msg_erro_email.removeAttribute("style");
+        msg_erro_email.classList.add("msg_erro");
+        msg_erro_email.innerHTML = 'O campo "email" não contém nada após o "@".';
 
         return_email = false;
     } else {
+        msg_erro_email.setAttribute("style", "display: none");
+        msg_erro_email.classList.remove("msg_erro");
+
         return_email = true;
     }
 
     habilitar_botao();
 });
 
-senha_valor.addEventListener('input', function() {
+senha_valor.addEventListener("input", function () {
     senha = this.value;
 
-    if (this.value.length == 0) {
-        // Mostre o erro de preencher o campo
-
-        return_senha = false;
+    if (this.value.length === 0) {
+        msg_erro_senha.setAttribute("style", "display: none");
+        msg_erro_senha.classList.remove("msg_erro");
     } else if (this.value.length < 8 || this.value.length > 20) {
         // Mostre o erro de que precisa estar entre 8 e 20 caracteres
+        msg_erro_senha.removeAttribute("style");
+        msg_erro_senha.classList.add("msg_erro");
+        msg_erro_senha.innerHTML = 'O campo "senha" deve ser preenchido com um número entre 8 e 20 caracteres.';
 
         return_senha = false;
-    } else if (this.value.indexOf(' ') !== -1) {
+    } else if (this.value.indexOf(" ") !== -1) {
         // Mostre o erro de que contém espaços em branco
+        msg_erro_senha.removeAttribute("style");
+        msg_erro_senha.classList.add("msg_erro");
+        msg_erro_senha.innerHTML = 'O campo "senha" contém espaços em branco.';
 
         return_senha = false;
     } else {
+        msg_erro_senha.setAttribute("style", "display: none");
+        msg_erro_senha.classList.remove("msg_erro");
+
         return_senha = true;
     }
 
     habilitar_botao();
 });
 
-confirmar_senha_valor.addEventListener('input', function() {
-    if (senha !== this.value) {
+confirmar_senha.addEventListener("input", function () {
+    if (this.value.length === 0) {
+        msg_erro_confirmar_senha.setAttribute("style", "display: none");
+        msg_erro_confirmar_senha.classList.remove("msg_erro");
+    } else if (senha !== this.value) {
         // Mostre o erro de que estão diferentes
+        msg_erro_confirmar_senha.removeAttribute("style");
+        msg_erro_confirmar_senha.classList.add("msg_erro");
+        msg_erro_confirmar_senha.innerHTML = 'A senha no campo "confirmar senha" está diferente da senha do campo "senha".';
 
         return_confirmar_senha = false;
     } else {
+        msg_erro_confirmar_senha.setAttribute("style", "display: none");
+        msg_erro_confirmar_senha.classList.remove("msg_erro");
+
         return_confirmar_senha = true;
     }
 
     habilitar_botao();
 });
 
- let habilitar_botao = () => {
-    console.log(return_nome_usuario);
-    console.log(return_email);
-    console.log(return_senha);
-    console.log(return_confirmar_senha);
-
+let habilitar_botao = () => {
     if (return_nome_usuario === true && return_email === true && return_senha === true && return_confirmar_senha === true) {
-        botao_cadastro.classList.add('botao_cadastro');
-
+        botao_cadastro.classList.add("botao_cadastro");
         botao_cadastro.removeAttribute("disabled");
-
-        console.log('if');
     } else {
-        botao_cadastro.classList.remove('botao_cadastro');
-
-        console.log('else');
+        botao_cadastro.classList.remove("botao_cadastro");
     }
-}
+};
